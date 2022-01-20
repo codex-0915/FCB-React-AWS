a2eca92740313a83961240cc232984409fee7df8
codex-0915 <kyl.perez@gmail.com>
2022-01-20 16:46:18 +0800

    Improve spacing and new Tasklist component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function TasksList(){
+	return(
+		<ul>
+          <li>Follow Edukasyon.ph on Facebook.</li>
+          <li>Follow AWS Siklab Pilipinas on Facebook.</li>
+          <li>Follow Zuitt Coding Bootcamp on Facebook.</li>
+      </ul>
+	)
+}
+
 function App() {
+	const paraStyle = {
+		"marginBottom": "15px"
+	};
   return (
     <div className="app">
       <header className="app-header">
         <h1>My Amazing ToDo-List App</h1>
-        <p>The most simple and amazing todo-list React app.</p>
+        <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
+      	<TasksList />
       </header>
     </div>
   );
