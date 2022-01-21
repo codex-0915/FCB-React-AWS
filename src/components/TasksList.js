b128b5bde52576d154ab5a4619fdc04272100a9a
codex-0915 <kyl.perez@gmail.com>
2022-01-21 15:44:30 +0800

    Organize the file and folder structure of components, final product

---
+++ src/components/TasksList.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Task from "./Task";
+
+function TasksList() {
+  const [taskItemsList, setTaskItemsList] = useState([
+    "Follow Edukasyon.ph on Facebook.", // 0
+    "Follow AWS Siklab Pilipinas on Facebook.", // 1
+    "Follow Zuitt Coding Bootcamp on Facebook." // 2
+  ]);
+
+
+  const [taskValue, setTaskValue] = useState("");
+
+  const inputChangeHandler = (e) => {
+    setTaskValue(e.target.value)
+  };
+
+  const addTaskHandler = () => {
+    setTaskItemsList([taskValue, ...taskItemsList]);
+    setTaskValue("");
+  };
+
+  return (
+    <>
+      <input
+        className="task-input"
+        placeholder="Create a new task"
+        onChange={inputChangeHandler}
+        onBlur={addTaskHandler}
+        value={taskValue}
+      />
+      <ul>
+        {taskItemsList.map((task, index) => {
+          return <Task key={index} taskName={task} />;
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default TasksList;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-function Task(props) {
-	console.log(props);
-	return (
-		<li className="tasks-item">{props.taskName}</li>
-	);
-}
-
-function TasksList(){
-	return(
-		<ul>
-         	<Task taskName="Follow Edukasyon.ph on Facebook."/>
-         	<Task taskName="Follow AWS Siklab Pilipinas on Facebook."/>
-         	<Task taskName="Follow Zuitt Coding Bootcamp on Facebook."/>
-    	</ul>
-	)
-}
+import TasksList from "./components/TasksList";
 
 function App() {
-	const paraStyle = {
-		"marginBottom": "15px"
-	};
+  const paraStyle = {
+    "marginBottom": "15px"
+  };
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>My Amazing ToDo-List App</h1>
-        <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
-      	<TasksList />
+        <p style={paraStyle} >The most simple and amazing todo-list React app.</p>
+        <TasksList />
       </header>
     </div>
   );
